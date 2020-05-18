35be46ec3c1cd990c3665851e817397b43dccf16
dongwq2 <dongwq2@lenovo.com>
2020-05-18 10:59:30 +0800

    feat(DEW-9) remove cache manager; add dependency ioredis

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,7 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from '../mongoose/mongoose-config.service';
 import { PassportModule } from '@nestjs/passport';
-import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
     imports: [
@@ -13,12 +12,12 @@ import * as redisStore from 'cache-manager-ioredis';
             defaultStrategy: 'local',
             session: false,
         }),
-        CacheModule.register({
-            store: redisStore,
-            host: 'r-8vbtu2cukh3iqdsxpqpd.redis.zhangbei.rds.aliyuncs.com',
-            port: 6379,
-            password: 'MhxzKhl$#%&'
-        })
+        // CacheModule.register({
+        //     store: redisStore,
+        //     host: 'r-8vbtu2cukh3iqdsxpqpd.redis.zhangbei.rds.aliyuncs.com',
+        //     port: 6379,
+        //     password: 'MhxzKhl$#%&'
+        // })
     ]
 })
 export class CoreModule {}
